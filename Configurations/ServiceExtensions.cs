5e408a887f280b7fd1d5ef055c421608e7c326dc
fomonaiye <93926794+fomonaiye@users.noreply.github.com>
2021-12-02 12:46:39 +0100

    Added Database and DTOs

---
+++ Configurations/ServiceExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using WebAPITemplate.Utililty.Configuration;
+
+namespace WebAPITemplate.Configurations
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureAutoMapper(this IServiceCollection services)
+        {
+            services.AddAutoMapper(typeof(MapperInitilizer));
+        }
+    }
+}
